ba68be16c5cbfa3a445e7640a2f003121f898516
B.V.K Ratnesh <ratneshbvk2504@gmail.com>
2024-10-17 00:51:42 +0530

    31 to 35

---
+++ src/_31_PascalTriangle.java
@@ -0,0 +1,42 @@
+/*Generating a Pascal’s Triangle
+Difficulty: Medium
+Topics: Arrays, Mathematical Computations
+Description: Write a program to generate Pascal's Triangle up to a given number of rows.
+Example:
+Input: rows = 4
+Output:
+        1
+        1 1
+        1 2 1
+        1 3 3 1
+Explanation: Pascal's Triangle with 4 rows is generated.
+ */
+
+//Link https://www.geeksforgeeks.org/pascal-triangle/
+
+import java.util.Scanner;
+
+public class _31_PascalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(PascalCoeff(i,j)+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static int PascalCoeff(int i,int j){
+        int res =1;
+        if(j> i-j){
+            j = i-j;
+        }
+        for (int k = 0; k < j; ++k)
+        {
+            res *= (i - k);
+            res /= (k + 1);
+        }
+        return res;
+    }
+}
---
+++ src/_34_Anagram.java
@@ -0,0 +1,60 @@
+/*Checking for an Anagram
+Difficulty: Easy
+Topics: String Manipulation
+Description: Write a program to check if two strings are anagrams.
+Example:
+Input: string1 = "listen", string2 = "silent"
+Output: True
+Explanation: "listen" and "silent" are anagrams of each other.
+ */
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class _34_Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        //Approach 1 Converting to array then sort, check for equaltiy
+       /* char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        if(Arrays.equals(s1,s2)){
+            System.out.println("Anagrams");
+        }else{
+            System.out.println("Not Anagrams");
+        }
+        */
+        //Approach 2 HashMap
+        if(AreAnagrams(str1,str2)){
+            System.out.println("Anagrams");
+        }else{
+            System.out.println("Not Anagrams");
+        }
+    }
+    public static boolean AreAnagrams(String s1,String s2){
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (char c : s1.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s2.toCharArray()) {
+            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
+                return false;
+            }
+            charCount.put(c, charCount.get(c) - 1);
+        }
+        for (int count : charCount.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
