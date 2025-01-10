22605698187884c06605c0fb3cb06caaeec40528
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-01-10 19:11:41 +0530

    s2 14 modified

--- src/Sprint_2/_14_ValidParenthesis.java
+++ src/Sprint_2/_14_ValidParenthesis.java
@@ -22,7 +22,7 @@ public class _14_ValidParenthesis {
         System.out.println(isValid(str));
     }
     public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
